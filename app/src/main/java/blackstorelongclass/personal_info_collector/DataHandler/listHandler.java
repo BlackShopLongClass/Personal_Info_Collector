597587d2092ecdbe76ff72a7f2c022ec8b276fa7
Od1gree <fakewall@protonmail.com>
2017-11-11 16:20:37 +0800

    add list handler

---
+++ app/src/main/java/blackstorelongclass/personal_info_collector/DataHandler/listHandler.java
@@ -0,0 +1,9 @@
+package blackstorelongclass.personal_info_collector.DataHandler;
+
+/**
+ * Created by abc123one on 2017/11/11.
+ */
+
+public class listHandler {
+
+}
--- app/src/main/java/blackstorelongclass/personal_info_collector/MainActivity.java
+++ app/src/main/java/blackstorelongclass/personal_info_collector/MainActivity.java
@@ -2,9 +2,12 @@ package blackstorelongclass.personal_info_collector;
 
 import android.icu.util.Calendar;
 import android.icu.util.GregorianCalendar;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,6 +15,7 @@ import blackstorelongclass.personal_info_collector.listMonitor.userList;
 import blackstorelongclass.personal_info_collector.listMonitor.userTag;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = this.getClass().getSimpleName();
 
     public final static String EXTRA_MESSAGE = "blackstorelongclass.personal_info_collector.MESSAGE";
 
@@ -28,15 +32,20 @@ public class MainActivity extends AppCompatActivity {
 //        intent.putExtra(Output,result);
 //        startActivity(intent);
         TextView textView = new TextView(this);
-        Calendar calendar = new GregorianCalendar(2017,02,23,18,29,51);
+        //Calendar calendar = new GregorianCalendar(2017,02,23,18,29,51);
         String result = "";
         long a = 11;
         userTag u = new userTag("number",a);
         userList list = new userList("test");
         list.addTag("number",u);
-        result = result+u.getClassType()+""+u.getObject();
-        textView.setText(result);
-        ConstraintLayout Label = (ConstraintLayout) findViewById(R.id.main);
-        Label.addView(textView);
+        Class<?> longType = java.lang.Long.class;
+        if(u.getClassType().equals(longType))
+            result = result+u.getClassType()+" "+u.getObject();
+        else
+            result = u.getClassType().toString() +" " + java.lang.Long.class.toString();
+        Log.e(TAG,result);
+        //textView.setText(result);
+        //ConstraintLayout Label = (ConstraintLayout) findViewById(R.id.main);
+        //Label.addView(textView);
     }
 }
