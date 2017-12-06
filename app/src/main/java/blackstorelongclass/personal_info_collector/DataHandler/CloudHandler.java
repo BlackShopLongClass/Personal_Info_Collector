ab40b97e01e1559a21c02e62c7766e23083975fb
The-water-tree <1241763690@qq.com>
2017-12-06 23:40:06 +0800

    修改原来的数据库相关代码以适应地点经纬度数据的加入

--- app/src/main/java/blackstorelongclass/personal_info_collector/DataHandler/CloudHandler.java
+++ app/src/main/java/blackstorelongclass/personal_info_collector/DataHandler/CloudHandler.java
@@ -3,7 +3,7 @@ package blackstorelongclass.personal_info_collector.DataHandler;
 /**
  * Created by ycd on 2017/12/5.
  */
-
+import org.json.*;
 public class CloudHandler {
     
 }
--- app/src/main/java/blackstorelongclass/personal_info_collector/DatebaseControler/DBHelper.java
+++ app/src/main/java/blackstorelongclass/personal_info_collector/DatebaseControler/DBHelper.java
@@ -32,6 +32,8 @@ public class DBHelper extends SQLiteOpenHelper {
         //创建数据表
         String SQL_createConfig="CREATE TABLE Config(ID INTEGER PRIMARY KEY AUTOINCREMENT, listName TEXT, tagType TEXT);";
         db.execSQL(SQL_createConfig);
+        String SQL_creatLink="CREAT TABLE Link(ID INTEGER PRIMARY KEY AUTOINCREMENT, list1Name TEXT, item1Time REAL, list2Name TEXT, item2Time REAL, tagName TEXT);";
+        db.execSQL(SQL_creatLink);
     }
 
     @Override
