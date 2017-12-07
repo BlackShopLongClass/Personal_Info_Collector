d961bc4748374fb8121b332a5b70a73846e52110
Od1gree <fakewall@protonmail.com>
2017-12-07 22:43:46 +0800

    fix create list bug

--- app/src/main/java/blackstorelongclass/personal_info_collector/listMonitor/userTag.java
+++ app/src/main/java/blackstorelongclass/personal_info_collector/listMonitor/userTag.java
@@ -78,7 +78,7 @@ public class userTag {
      * @return bool
      */
     public boolean isCalendar(){
-        if(C == java.util.GregorianCalendar.class) return true;
+        if(C == java.util.Calendar.class) return true;
         else if(C == java.lang.Long.class) return true;
         else return false;
     }
